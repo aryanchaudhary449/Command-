2aeed39028e9e30b02c6897493c9648311585f81
triednow <158821779+triednow@users.noreply.github.com>
2024-09-18 14:14:30 +0530

    Update gdrive.py

--- gdrive.py
+++ gdrive.py
@@ -1,8 +1,9 @@
 ofn = "STAR GOLD HD [00:00-04:00].[15-09-2024] 1080p TPLAY WEB-DL x264 join @tataplay_ripping.mkv"
 msg = app.send_message(7126874550, "UploadIng")
 import time
+from bot.helpers.upload.gdrive import GoogleDriveUploader
 srt = time.time()
-upload_path = "/BotUploads/"
+upload_path = "BotUploads/files/"
 uploader = GoogleDriveUploader(app, msg, srt)
-uploader.upload_file(out_file_name, upload_path, ott=self.ott)
+uploader.upload_file(out_file_name, upload_path)
             
